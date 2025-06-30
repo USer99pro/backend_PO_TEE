dd902189ee38e051bb09d0248f69dc0f315a6983
USer99pro <akbbna48@gmail.com>
2025-06-30 19:17:30 +0700

    update 30-6-25

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -5,3 +5,14 @@ exports.showAllStudents =   async  ( req , res , next   )=> {
         const  result    =  await  Student.find();
         res.json({  studentData : result }) ;
 }
+
+
+exports.showStudentsByID =  async (req , res, next) =>{
+        const {id} = req.params;
+        const std = await Student.findById(id)
+        res.json({ data:std})
+}
+
+exports.InsertStudent =  async (req , res, next) =>{
+        const { name, age, sex, email} = req.body;
+}
--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -4,5 +4,6 @@ const router  = express.Router();
 const studentController = require('../controllers/studentController');
 
 router.get( '/'   ,  studentController.showAllStudents   );
+router.get('/:id' , studentController.showStudentsByID)
 
 module.exports = router;
